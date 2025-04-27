9fd8ff7e7869184319959e136a7d790e91625f20
Manu Cornet <m@ma.nu>
2025-04-27 08:57:18 +0000

    Fix-popup

--- statesread.js
+++ statesread.js
@@ -1,55 +1,55 @@
 const STATES = {
-  'AL': 'Alabama',
-  'AK': 'Alaska',
-  'AZ': 'Arizona',
-  'AR': 'Arkansas',
-  'CA': 'California',
-  'CO': 'Colorado',
-  'CT': 'Connecticut',
-  'DC': 'District of Columbia',
-  'DE': 'Delaware',
-  'FL': 'Florida',
-  'GA': 'Georgia',
-  'HI': 'Hawaii',
-  'ID': 'Idaho',
-  'IL': 'Illinois',
-  'IN': 'Indiana',
-  'IA': 'Iowa',
-  'KS': 'Kansas',
-  'KY': 'Kentucky',
-  'LA': 'Louisiana',
-  'ME': 'Maine',
-  'MD': 'Maryland',
-  'MA': 'Massachusetts',
-  'MI': 'Michigan',
-  'MN': 'Minnesota',
-  'MS': 'Mississippi',
-  'MO': 'Missouri',
-  'MT': 'Montana',
-  'NE': 'Nebraska',
-  'NV': 'Nevada',
-  'NH': 'New Hampshire',
-  'NJ': 'New Jersey',
-  'NM': 'New Mexico',
-  'NY': 'New York',
-  'NC': 'North Carolina',
-  'ND': 'North Dakota',
-  'OH': 'Ohio',
-  'OK': 'Oklahoma',
-  'OR': 'Oregon',
-  'PA': 'Pennsylvania',
-  'RI': 'Rhode Island',
-  'SC': 'South Carolina',
-  'SD': 'South Dakota',
-  'TN': 'Tennessee',
-  'TX': 'Texas',
-  'UT': 'Utah',
-  'VT': 'Vermont',
-  'VA': 'Virginia',
-  'WA': 'Washington',
-  'WV': 'West Virginia',
-  'WI': 'Wisconsin',
-  'WY': 'Wyoming',
+  AL: "Alabama",
+  AK: "Alaska",
+  AZ: "Arizona",
+  AR: "Arkansas",
+  CA: "California",
+  CO: "Colorado",
+  CT: "Connecticut",
+  DC: "District of Columbia",
+  DE: "Delaware",
+  FL: "Florida",
+  GA: "Georgia",
+  HI: "Hawaii",
+  ID: "Idaho",
+  IL: "Illinois",
+  IN: "Indiana",
+  IA: "Iowa",
+  KS: "Kansas",
+  KY: "Kentucky",
+  LA: "Louisiana",
+  ME: "Maine",
+  MD: "Maryland",
+  MA: "Massachusetts",
+  MI: "Michigan",
+  MN: "Minnesota",
+  MS: "Mississippi",
+  MO: "Missouri",
+  MT: "Montana",
+  NE: "Nebraska",
+  NV: "Nevada",
+  NH: "New Hampshire",
+  NJ: "New Jersey",
+  NM: "New Mexico",
+  NY: "New York",
+  NC: "North Carolina",
+  ND: "North Dakota",
+  OH: "Ohio",
+  OK: "Oklahoma",
+  OR: "Oregon",
+  PA: "Pennsylvania",
+  RI: "Rhode Island",
+  SC: "South Carolina",
+  SD: "South Dakota",
+  TN: "Tennessee",
+  TX: "Texas",
+  UT: "Utah",
+  VT: "Vermont",
+  VA: "Virginia",
+  WA: "Washington",
+  WV: "West Virginia",
+  WI: "Wisconsin",
+  WY: "Wyoming",
 };
 const stateCodes = Object.keys(STATES);
 
@@ -75,15 +75,15 @@ const processProgramData = (raw) => {};
 
 let stateEls = [];
 
-const HIGHLIGHT_CLASS = 'highlight';
+const HIGHLIGHT_CLASS = "highlight";
 const POPUP_ANCHOR_OFFSET = 40;
 const POPUP_WIDTH = 300;
 const POPUP_HEIGHT = 300;
 const POPUP_WINDOW_SAFETY_PADDING = 20;
 
 const isStateElement = (el) => {
-  const id = el.getAttribute('id');
-  return id !== '' && stateCodes.includes(id.toUpperCase());
+  const id = el.getAttribute("id");
+  return id !== "" && stateCodes.includes(id.toUpperCase());
 };
 
 const onMapHover = (e) => {
@@ -95,10 +95,9 @@ const onMapHover = (e) => {
     showPopup(false);
     return;
   }
-  let id = e.target.getAttribute('id');
+  let id = e.target.getAttribute("id");
   const state = e.target;
-  let currentlyHighlighted =
-      document.getElementsByClassName(HIGHLIGHT_CLASS);
+  let currentlyHighlighted = document.getElementsByClassName(HIGHLIGHT_CLASS);
   if (currentlyHighlighted.length > 0) {
     for (let i = 0; i < currentlyHighlighted.length; i++) {
       currentlyHighlighted[i].classList.remove(HIGHLIGHT_CLASS);
@@ -117,48 +116,52 @@ const getPopupTitle = (id) => {
 };
 
 const showPopup = (show, clientX, clientY, id) => {
-  const el = document.getElementById('popup');
-  el.style.display = show ? 'block' : 'none';
+  const el = document.getElementById("popup");
+  el.style.display = show ? "block" : "none";
   if (show) {
     let x, y;
-    let overflowX = (clientX + POPUP_WIDTH >= window.innerWidth -
-                     POPUP_WINDOW_SAFETY_PADDING);
-    let overflowY = (clientY + POPUP_HEIGHT >= window.innerHeight -
-                     POPUP_WINDOW_SAFETY_PADDING);
+    let overflowX =
+      clientX + POPUP_WIDTH >= window.innerWidth - POPUP_WINDOW_SAFETY_PADDING;
+    let overflowY =
+      clientY + POPUP_HEIGHT >=
+      window.innerHeight - POPUP_WINDOW_SAFETY_PADDING;
 
     if (overflowX) {
       x = clientX - POPUP_WIDTH - POPUP_ANCHOR_OFFSET;
     } else {
       x = clientX + POPUP_ANCHOR_OFFSET;
-    }let assessmentData;
+    }
+    if (overflowY) {
+      y = clientY - POPUP_HEIGHT - POPUP_ANCHOR_OFFSET;
+    } else {
+      y = clientY + POPUP_ANCHOR_OFFSET;
+    }
+
+    let assessmentData;
     let programData;
 
+    el.style.top = "" + y + "px";
+    el.style.left = "" + x + "px";
+    el.style.width = POPUP_WIDTH + "px";
+    el.style.height = POPUP_HEIGHT + "px";
 
-    el.style.top = '' + y + 'px';
-    el.style.left = '' + x + 'px';
-    el.style.width = POPUP_WIDTH + 'px';
-    el.style.height = POPUP_HEIGHT + 'px';
-
-    el.innerHTML = getPopupTitle(id) + '<br/><br/>';
+    el.innerHTML = getPopupTitle(id) + "<br/><br/>";
   }
-}
-
+};
 
 const processMap = (mapEl) => {
-  const allPaths = [
-    ...mapEl.querySelectorAll('path')
-  ];
-  stateEls = allPaths.filter(p => isStateElement(p));
+  const allPaths = [...mapEl.querySelectorAll("path")];
+  stateEls = allPaths.filter((p) => isStateElement(p));
 };
 
 const onMapLoad = async () => {
-  const mapEl = document.getElementById('map');
-  const response = await fetch('states.svg');
+  const mapEl = document.getElementById("map");
+  const response = await fetch("states.svg");
   const svgData = await response.text();
 
-  const assessmentResponse = await fetch('data/assessments.txt');
+  const assessmentResponse = await fetch("data/assessments.txt");
   const assessmentData = await assessmentResponse.text();
-  const programResponse = await fetch('data/programs.txt');
+  const programResponse = await fetch("data/programs.txt");
   const programData = await programResponse.text();
 
   processAssessmentData();
@@ -169,5 +172,5 @@ const onMapLoad = async () => {
 
   mapEl.innerHTML = svgData;
   processMap(mapEl);
-  mapEl.addEventListener('mouseover', onMapHover);
+  mapEl.addEventListener("mouseover", onMapHover);
 };
