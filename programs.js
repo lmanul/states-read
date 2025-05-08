3fc580fb8193fb9d2de566a3fc6bb6c9c0f551d8
Manu Cornet <m@ma.nu>
2025-05-08 09:06:22 +0000

    Show aligned, feasible

--- programs.js
+++ programs.js
@@ -14,6 +14,8 @@ class Program {
     this.name = name;
     this.approvingStates = [];
     this.company = null;
+    this.aligned = null;
+    this.feasible = null;
   }
 }
 
@@ -22,17 +24,21 @@ export const processProgramData = (raw) => {
   const programBlocks = raw.split("\n\n\n");
   for (let block of programBlocks) {
     const keyValues = block.split("\n");
-    const program = new Program('noid', 'Unknown Program');
+    const program = new Program("noid", "Unknown Program");
     let id = null;
     for (let keyValue of keyValues) {
-      let [key, value] = keyValue.split("|").map(a => a.trim());
+      let [key, value] = keyValue.split("|").map((a) => a.trim());
       if (key === "id") {
         id = value;
         program.id = id;
       } else if (key === "name") {
         program.name = value;
-      } else if (key === 'company') {
+      } else if (key === "company") {
         program.company = value;
+      } else if (key === "aligned") {
+        program.aligned = value;
+      } else if (key === "feasible") {
+        program.feasible = value;
       } else if (key === "states") {
         const approvingStates = value.trim().split(",");
         for (let stateCode of approvingStates) {
@@ -48,14 +54,38 @@ export const processProgramData = (raw) => {
   }
 };
 
+const getValueForFeasible = (id) => {
+  switch (id) {
+    case "me":
+      return "Meets EdReports expectations";
+    default:
+      return "";
+  }
+};
+
+const getValueForAligned = (id) => {
+  switch (id) {
+    case "me":
+      return "Meets EdReports expectations";
+    case "pa":
+      return "Partially Meets EdReports expectations";
+    default:
+      return "";
+  }
+};
+
 const formatProgramDetails = (id) => {
   const program = programs[id];
+  const aligned = getValueForAligned(program.aligned);
+  const feasible = getValueForFeasible(program.feasible);
   return `
       <h1 class="program">${program.name}</h1>
       <p><b>Approved in ${program.approvingStates.length} states: </b>
       ${program.approvingStates.map(formatSingleStatePill).join(" ")}
       </p>
-      ${program.company ? "<p><b>Company</b>: " + program.company + "</p>" : ''}
+      ${program.company ? "<p><b>Company</b>: " + program.company + "</p>" : ""}
+      ${aligned ? "<p><b>Aligned</b>: " + aligned + "</p>" : ""}
+      ${feasible ? "<p><b>Feasible</b>: " + feasible + "</p>" : ""}
     `;
 };
 
