aebae85208a19e5ab3199be1e87f23b335fd9643
Manu Cornet <m@ma.nu>
2025-05-08 08:16:47 +0000

    Forgot to add u tils

---
+++ util.js
@@ -0,0 +1,34 @@
+const HIGHLIGHT_CLASS = "highlight";
+
+export const setDetails = (markup) => {
+  document.getElementById("details").innerHTML = markup;
+};
+
+export const clearDetails = () => {
+  setDetails("");
+};
+
+export const clearStateHighlights = () => {
+  let currentlyHighlighted = document.getElementsByClassName(HIGHLIGHT_CLASS);
+  while (currentlyHighlighted.length > 0) {
+    for (let i = 0; i < currentlyHighlighted.length; i++) {
+      currentlyHighlighted[i].classList.remove(HIGHLIGHT_CLASS);
+    }
+    currentlyHighlighted = document.getElementsByClassName(HIGHLIGHT_CLASS);
+  }
+};
+
+export const highlightStates = (stateCodes, states) => {
+  clearStateHighlights();
+  for (let stateCode of stateCodes) {
+    states[stateCode].element.classList.add(HIGHLIGHT_CLASS);
+  }
+};
+
+export const formatSingleStatePill = (id) => {
+    return `
+      <div class="pill state-pill" onclick="">${id}</div>
+    `;
+  };
+
+  window.clearStateHighlights = clearStateHighlights;
