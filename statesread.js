329bd000a431c69b4da4052f3c66f78a70b17842
Manu Cornet <m@ma.nu>
2025-05-12 17:28:34 +0000

    Restore map hovering

--- statesread.js
+++ statesread.js
@@ -7,6 +7,11 @@ import { formatSingleProgramPill, processProgramData } from "./programs.js";
 import { init as statesInit, states } from "./states.js";
 import { clearStateHover, clearStateSelected, HOVER_CLASS, SELECTED_CLASS } from "./util.js";
 
+const POPUP_ANCHOR_OFFSET = 1;
+const POPUP_WIDTH = 400;
+const POPUP_HEIGHT = 400;
+const POPUP_WINDOW_SAFETY_PADDING = 20;
+
 const init = () => {
   statesInit();
   document.body.onload = onMapLoad;
@@ -17,7 +22,7 @@ const isStateElement = (el) => {
   return id !== "" && stateCodes.includes(id.toUpperCase());
 };
 
-const onMapClick = (e) => {
+const onMapHover = (e) => {
   if (!states["CA"].element) {
     // No data yet.
     return;
@@ -60,6 +65,28 @@ const showPopup = (show, clientX, clientY, id) => {
   const el = document.getElementById("popup");
   el.style.display = show ? "block" : "none";
   if (show) {
+    let x, y;
+    let overflowX =
+      clientX + POPUP_WIDTH >= window.innerWidth - POPUP_WINDOW_SAFETY_PADDING;
+    let overflowY =
+      clientY + POPUP_HEIGHT >=
+      window.innerHeight - POPUP_WINDOW_SAFETY_PADDING;
+
+    if (overflowX) {
+      x = clientX - POPUP_WIDTH - POPUP_ANCHOR_OFFSET;
+    } else {
+      x = clientX + POPUP_ANCHOR_OFFSET;
+    }
+    if (overflowY) {
+      y = clientY - POPUP_HEIGHT - POPUP_ANCHOR_OFFSET;
+    } else {
+      y = clientY + POPUP_ANCHOR_OFFSET;
+    }
+
+    el.style.top = "" + y + "px";
+    el.style.left = "" + x + "px";
+    el.style.width = POPUP_WIDTH + "px";
+    el.style.height = POPUP_HEIGHT + "px";
     const stateCode = id.toUpperCase();
     el.innerHTML = formatPopupContent(stateCode);
   }
@@ -143,11 +170,7 @@ const onMapLoad = async () => {
 
   mapEl.innerHTML = svgData;
   processMap(mapEl);
-  mapEl.addEventListener("click", onMapClick);
-  mapEl.addEventListener("mouseover", (e) => {
-    clearStateHover();
-    e.target.classList.add(HOVER_CLASS);
-  });
+  mapEl.addEventListener("mousemove", onMapHover);
   document.body.addEventListener("click", (e) => {
     if (!elementIsInMap(e.target)) {
       clearStateHover();
